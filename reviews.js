1df08872c7c4cc5092e25a6348aa5c0c2f0ac15f
AlekOmOm <alekellegard@protonmail.com>
2025-07-17 17:49:40 +0200

    ﻿ chore: 🧹

--- reviews.js
+++ reviews.js
@@ -16,25 +16,5 @@ window.REVIEWS = [
     "comment": "",
     "date": "2025-07-17",
     "id": 2
-  },
-  {
-    "place": "test2",
-    "dish": "ttes",
-    "rating": 4,
-    "reviewer": "alek",
-    "comment": "",
-    "category": "street-food",
-    "date": "2025-07-17",
-    "id": 3
-  },
-  {
-    "place": "fds",
-    "dish": "fdsa",
-    "rating": 5,
-    "reviewer": "Kevin",
-    "comment": "d",
-    "category": "street-food",
-    "date": "2025-07-17",
-    "id": 4
   }
 ];
--- reviews/2025-07-16_reload_falafel.md
+++
@@ -1,8 +0,0 @@
-place: Reload
-dish: Falafel dish
-rating: 7.5
-reviewer: william
-comment:
-
----
-*Review #001 - July 16, 2025*
